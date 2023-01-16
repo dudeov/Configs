064840b3047288571aff12bd698e6795c7f4e09b
Alex Chuvakov <chuvakov.bca@gmail.com>
2023-01-16 20:50:07 +0000

    Update init.lua, removing broken colorscheme

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -6,7 +6,6 @@ else
     require "user.options"
     require "user.keymaps"
     require "user.plugins"
-    require "user.colorscheme"
     require "user.cmp"
     require "user.lsp"
     require "user.telescope"
